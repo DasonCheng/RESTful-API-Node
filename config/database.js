00008b988a63f50a5249d1eba7fb8babcafa5dca
Origin <dasoncheng@foxmail.com>
2016-09-26 17:38:20 +0800

    update

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
-module.exports = {
-    conf: {
-        host: 'localhost',
-        user: 'origin',
-        password: 'Origin1314user..',
-        database:'blog_myour'
-    }
-};
+var mysql = require('mysql');
+var pool = mysql.createPool({
+    host: 'localhost',
+    user: 'origin',
+    password: 'Origin1314user..',
+    database: 'blog_myour'
+});
+module.exports = pool;
